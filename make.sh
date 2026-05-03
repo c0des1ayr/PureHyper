4a8697271df74be6af7f55a30b10dee9d129e5cb
Alex Polo <apolitis006@outlook.com>
2026-05-03 02:20:36 -0500

    Make addon compilation a bit more portable

--- make.sh
+++ make.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # this damn engine needs zips with no sparse files, so I can't use bsdtar
+ADDONFILE="VL_PureHyper_v0.0.2.pk3"
 TMPDIR=$(mktemp -d)
 WORKDIR=$PWD
 pushd files
 tar -cvf - . | tar -xf - -C $TMPDIR
 popd
 pushd $TMPDIR
-zip -r VL_PureHyper_v0.0.2.pk3 .
-mv VL_PureHyper_v0.0.2.pk3 $WORKDIR
+zip -r $ADDONFILE .
+mv $ADDONFILE $WORKDIR
 popd
 rm -rf $TMPDIR
